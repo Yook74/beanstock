481bc48deb300d2aca1dfb904883703429cfb1d1
Andrew Blomenberg <andrewBlomen@gmail.com>
2025-10-23 20:26:05 -0600

    RIP henchmen kittie

--- routes/root.py
+++ routes/root.py
@@ -7,4 +7,10 @@ bluep = Blueprint('root', __name__, url_prefix='/')
 
 @bluep.get('')
 def home():
-    return render_template('home.html')
+    items = Item.query.all()
+
+    total_value = 0
+    for item in items:
+        total_value += item.standard_price * item.quantity
+
+    return render_template('home.html', total_value=total_value, items=items)
