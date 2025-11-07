5263acd1e8d263bf769973c91be8c88702aaae67
Sam Jorge <samanthajorge2006@icloud.com>
2025-11-06 19:49:05 -0700

    homepage sucks slightly less

--- routes/root.py
+++ routes/root.py
@@ -7,10 +7,12 @@ bluep = Blueprint('root', __name__, url_prefix='/')
 
 @bluep.get('')
 def home():
-    items = Item.query.all()
+    types = ItemType.query.all()
 
     total_value = 0
-    for item in items:
-        total_value += item.standard_price * item.quantity
+    for type in types:
+        for item in type.items:
+            if item.standard_price and item.quantity:
+                total_value += item.standard_price * item.quantity
 
-    return render_template('home.html', total_value=total_value, items=items)
+    return render_template('home.html', total_value=total_value, item_types=types)
